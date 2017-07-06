48a70490c2f9c795c720547803eafe5cb8eaa3fa
Bibek Ghimire <bibek@stanza.co>
2017-07-05 20:52:55 -0700

    Threw in UnderConstructionPage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import UnderConstructionPage from './UnderConstructionPage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UnderConstructionPage />, document.getElementById('root'));
 registerServiceWorker();
