da71a65ba844e8e6170735fc104d3cc3841b32ec
Simon Zhou <simon.zhou@ucla.edu>
2017-07-16 14:51:58 -0700

    started staff member page

---
+++ src/components/StaffMember.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './StaffMember.scss';
+
+class StaffMember from React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    );
+  }
+
+}
---
+++ src/components/StaffPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './StaffPage.scss';
+
+class StaffPage extends React.Component {
+  constructor() {
+    
+  }
+
+
+  render() {
+    return (
+
+
+
+    );
+  }
+}
