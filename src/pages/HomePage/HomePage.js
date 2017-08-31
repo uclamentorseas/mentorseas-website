56267f49546f83c6949ef647519c4ecc75348536
Bibek <bibekg@ucla.edu>
2017-08-31 10:34:11 -0700

    Embed a StanzaCal below the Hero

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import * as React from 'react'
 
 import Hero from 'views/Hero'
 import TitledParagraphs from 'components/TitledParagraphs'
+import StanzaCal from 'views/StanzaCal'
 import { getSomeUclaImages } from 'images'
 import copy from './copy'
 import './HomePage.scss'
@@ -15,6 +16,11 @@ export default (): React.Element<*> => {
     <div className='page home-page'>
 
       <Hero />
+
+      <div className='home-page-stanza-cal'>
+        <h1>Our Upcoming Events</h1>
+        <StanzaCal />
+      </div>
 
       <div className='home-page-contents'>
         {
---
+++ src/views/StanzaCal/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <div
+    className='stanzacal'
+    data-stanzacal='uclamentorseas'
+    data-width='100%'
+    data-height='250px'
+  />
+)
