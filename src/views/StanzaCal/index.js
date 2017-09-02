a79d0a98f896f33f4e94f750102dc1042be4302d
Bibek <bibekg@ucla.edu>
2017-09-02 11:17:00 -0700

    Colocate StanzaCal script with component

--- src/views/StanzaCal/index.js
+++ src/views/StanzaCal/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+// @flow
 
-export default () => (
-  <div
-    className='stanzacal'
-    data-stanzacal='uclamentorseas'
-    data-width='100%'
-    data-height='250px'
-  />
-)
+import * as React from 'react'
+
+const STANZA_CAL_EMBED_URL = 'https://dff2h0hbfv6w4.cloudfront.net/scripts/embed-stanzacal-v1.js'
+
+type PropsType = {};
+
+export default class StanzaCal extends React.Component<PropsType> {
+  componentWillMount() {
+    const script = document.createElement('script')
+    script.setAttribute('src', STANZA_CAL_EMBED_URL)
+    if (document.body) {
+      document.body.appendChild(script)
+    }
+  }
+
+  render(): React.Element<*> {
+    return (
+      <div
+        className='stanzacal'
+        data-stanzacal='uclamentorseas'
+        data-width='100%'
+        data-height='250px'
+      />
+    )
+  }
+}
