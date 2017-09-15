979a3e382263471f3cfac550a804e0d3da9b79b7
Simon Zhou <simon.zhou@ucla.edu>
2017-09-15 15:28:27 -0700

    more changes

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,15 @@
+// @flow
+
+import * as React from 'react'
+import header from 'pages/StaffPage/images/header.svg'
+import './PageHeader.scss'
+
+export default (): React.Element<*> => (
+  <div className='page-header'>
+    <div className='title'>
+      <div className='title-header'>
+        <img className='page-header-img' alt='page header img' src={header} />
+      </div>
+    </div>
+  </div>
+)
