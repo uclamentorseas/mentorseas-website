66a933fd1f21fd027d2ff38d21b9a95045f18993
Bibek <bibekg@ucla.edu>
2018-04-28 15:10:25 -0700

    Change Google Form link to CCLE and add placeholder images

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -18,26 +18,63 @@ const StepsWrapper = styled.div`
   }
 `
 
-export default function SignUpPage(): React.Element<*> {
+const TutorialImage = styled.img`
+  max-height: 300px;
+`
 
+export default function SignUpPage(): React.Element<*> {
   return (
-    <div className='page'>
-      <div className='page-contents'>
-        <SectionHeader allowClickShapes={false} title='Become a Mentor!' background={colors.mint} />
+    <div className="page">
+      <div className="page-contents">
+        <SectionHeader
+          allowClickShapes={false}
+          title="Become a Mentor!"
+          background={colors.mint}
+        />
 
         <TextWrapper>
-          <Title color={colors.red}>Sign up to become a 2018-2019 mentorSEAS mentor!</Title>
-          <Subtitle>{'It only takes a few minutes and it\'s as easy as 1, 2, 3...'}</Subtitle>
+          <Title color={colors.red}>
+            Sign up to become a 2018-2019 mentorSEAS mentor!
+          </Title>
+          <Subtitle>
+            {"It only takes a few minutes and it's as easy as 1, 2, 3..."}
+          </Subtitle>
 
           <StepsWrapper>
-            <Text size={18}>1. Log on to <a target='_blank' rel='noopener noreferrer' href='https://my.engineering.ucla.edu'>my.engineering.ucla.edu</a> {'and click on "About Me" under "My Profile".'}</Text>
-            <Text size={18}>{'2. In the "Mentor Profile" section, fill in the information, agree to the agreements and click "Sign up to be a mentor".'}</Text>
             <Text size={18}>
-              3. Fill out the additional mentee matching <a target='_blank' rel='noopener noreferrer' href='https://goo.gl/forms/ifNvt8SADELmjhqD3'>Google Form</a>, which is also linked to on myEngineering.
+              1. Log on to{' '}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://my.engineering.ucla.edu"
+              >
+                my.engineering.ucla.edu
+              </a>{' '}
+              {'and click on "About Me" under "My Profile".'}
             </Text>
+            <TutorialImage alt="1" src="https://placehold.it/800x600" />
+            <Text size={18}>
+              {
+                '2. In the "Mentor Profile" section, fill in the information, agree to the agreements and click "Sign up to be a mentor".'
+              }
+            </Text>
+            <TutorialImage alt="1" src="https://placehold.it/800x600" />
+            <Text size={18}>
+              3. Fill out the additional mentee matching{' '}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://ccle.ucla.edu/mod/feedback/view.php?id=1983190"
+              >
+                survey on CCLE
+              </a>, which is also linked to on myEngineering.
+            </Text>
+            <TutorialImage alt="1" src="https://placehold.it/800x600" />
           </StepsWrapper>
 
-          <Subtitle>{'That\'s it! Once you\'ve completed these steps, you\'re all set. 🤗'}</Subtitle>
+          <Subtitle>
+            {"That's it! Once you've completed these steps, you're all set. 🤗"}
+          </Subtitle>
         </TextWrapper>
       </div>
     </div>
